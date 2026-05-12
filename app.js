9478138fdb48ba79a5e952529f5a6ca0b618fae8
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-05-12 14:50:32 +0530

    code updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    console.log("Hello 2");
+    console.log("Hello 3");
     res.send('Hello from Jenkins CI/CD!');
 });
 
