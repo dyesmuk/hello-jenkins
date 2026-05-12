8599220baf357d05a8520b0b8d8360d923440e73
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-05-12 15:39:06 +0530

    code updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from Jenkins CI/CD! 12345 9999999');
+    res.send('Hello from Jenkins CI/CD! 12345');
 });
 
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -1,8 +1,12 @@
 const request = require('supertest');
 const app = require('./app');
 
-test('GET / returns greeting', async () => {
+test('GET / returns 200', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toContain('Hello from Jenkins CI/CD!');
+});
+
+test('GET / returns hello', async () => {
+    const res = await request(app).get('/');
+    expect(res.text).toContain('Hello');
 });
